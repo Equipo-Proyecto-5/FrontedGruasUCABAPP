93230b3d817729ea97da1a7be850eb21ac3fd319
Rymedero <rymedero.21@est.ucab.edu.ve>
2025-01-11 19:59:00 -0400

    Se realizo la interfaz de cambio de contraseñas y se configuraron rutas

--- app/tabs/profile.tsx
+++ app/tabs/profile.tsx
@@ -1,12 +1,20 @@
 import { View, Text, StyleSheet } from "react-native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Profile from "../../pages/Profile/Profile";
+import ChangePassword from "../../pages/Profile/ChangePassword";
+	
+
+const Stack = createNativeStackNavigator();
 
 
 export default function profile() {
     return (
-        <View style={style.container}>
-            <Text>Profile</Text>
-        </View>
-    );
+            <Stack.Navigator id={undefined}>
+                <Stack.Screen name="Profile" component={Profile} /> 
+                <Stack.Screen name="ChangePassword" component={ChangePassword} />                     
+            </Stack.Navigator> 
+                     
+                    );
 }
 
 
